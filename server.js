32f105571812cb089baa99ddee166a02c46b5abb
jpardike2 <jpardike@gmail.com>
2020-10-05 14:21:11 -0700

    Add index.ejs

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // Configuration
 const express = require('express');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 const PORT = 4000;
 
 // View Engine
 app.set('view engine', 'ejs');
+
+// Controller
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+})
 
 
 // Listener
