47f758174c4d729b4fa053687d9ac13b0939565f
jpardike2 <jpardike@gmail.com>
2020-10-05 14:34:54 -0700

    Set up animalController and add a basic array for testing models

---
+++ controllers/animalController.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+// Model
+const animal = require('../models/Animal');
+
+
+module.exports = router;
---
+++ models/Animal.js
@@ -0,0 +1,16 @@
+const Animal = [
+  {
+    name: 'Bear',
+    color: 'black',
+    region: 'North America',
+    hungry: true,
+  },
+  {
+    name: 'Otter',
+    color: 'brown',
+    region: 'Worldwide',
+    hungry: false,
+  },
+];
+
+module.exports = Animal;
